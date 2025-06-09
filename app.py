2ba50d510e772dc58c0738db703a41fb723b863c
Sadanksh Gangrade <sadankshzz@hotmail.com>
2025-06-09 07:59:12 +0000

    Final Build

--- app.py
+++ app.py
@@ -1,107 +1,119 @@
-# Importing Random Module to random generate choices
+# Impoorting Random  Module to grnerate choices and numbers
 import random
- 
-# Initializing two empty list for two categories of numbers
+
+# Initializong Empty list to store numbers in two categories(Red and Black)
 RED=[]
 BLACK=[]
 
 
-# Adding Even Numbers to Red and Odd to Black Category List
-for i in range(0,101):
+# Adding EVEN numbers to the Red list and ODD numbers to the Black List
+for i in range(1,101):
     if i%2==0:
         RED.append(i)
     else:
         BLACK.append(i)
 
 
-# Deposit Function to take deposits from the User
+# Function to take deposit from the Player
 def deposit():
-    amount=(input("Enter the amount to deposit: "))
-    if amount.isdigit():
-        amount=int(amount)
-    else:
-        print("Invalid amount")
+    while True:
+        amount=(input("Enter the amount to deposit: "))
+        if amount.isdigit():
+            amount=int(amount)
+            if amount>0:
+                break
+            else:
+                print("Amount must be greater than 0")
+        else:
+            print("Invalid amount")
+
     return amount
 
 
-# Updates the wallet based on previous activities
+# Function to update the wallet balance after every activity.
 def update_balance(balance,bet,winnings):
-    balance=balance-bet+winnings
+    if winnings>0:
+        balance=balance-bet+winnings
+    else:
+        balance=balance-bet
     return balance
 
 
-# Asks for the Betting Style the user want to have and return effective multiplier
+# Function to set the Multiplier based on the Player's choice of the betting range
 def bet_style():
     multiplier =1
-    style=input("Enter your bet style: ")
-    if  style.lower()=="low":
-        multiplier =0.5
-    elif style.lower()=="high":
-        multiplier =1.5
-    else:
-        print("Invalid bet style")
-    return multiplier
+    while True:
+        style=input("Enter your bet style: ")
+        if  style.lower()=="low":
+            multiplier =1.5
+        elif style.lower()=="high":
+            multiplier =3
+        else:
+            print("Invalid bet style")
+            bet_style()
+        return multiplier
 
 
-# Takes the Betting Number from the User
+# Function to set the num that player choosess to play bet on
 def get_number():
-    num=input("Enter the number: ")
-    if num.isdigit():
-        num=int(num)
-    else:
-        print("Invalid number")
+    while True:
+        num=input("Enter the number: ")
+        if num.isdigit():
+            num=int(num)
+            break
+        else:
+            print("Invalid number")
+
     return num
 
 
-# Takes the BET Amount from the User
+# Function to take the Bet Amount from the Player and cross examine it with the current wallet balance.
 def place_bet(balance):
-    bet=input("Enter your bet: ")
-    if bet.isdigit():
-        bet=int(bet)
-        if 0<bet<=balance:
-            pass
-        elif bet>balance:
-            print("Not enough balance")
+    while True:
+        bet=input("Enter your bet: ")
+        if bet.isdigit():
+            bet=int(bet)
+            if 0<bet<=balance:
+                break
+            elif bet>balance:
+                print("Not enough balance")
+
         else:
             print("Invalid bet")
-    else:
-        print("Invalid bet")
+
     return bet
 
 
-# Randomly genrates the Roll of the dice using the Random Module imported previously
+# Function to Roll the Dice randomly using the Random Module imported above
 def dice_roll():
     return random.randint(1,6)
 
-
-# Randomdly Spins the Roulette using the Random Module.
+# Function to spin the Roulette using the Random Module.
 def roulette_spin():
-   # If first chooses between the two lists randomly
+#    It first selects a choice randomly choosing between Red and Black and then choosing a number randomly from that list.
    choice = random.randint(1,2)
-   # Then chooses the number fromt eh selcted randomly.
    if choice==1:
        return random.choice(RED)
    else:
        return random.choice(BLACK)
 
 
-# Defines the winning conditions.
+# Function to set the Rules for the winning conditions and calculating the winnings.
 def winning_conditions(bet,multiplier,roll,spin,num,balance):
     winnings=0
-    # If the roll of the Dice is 1 or 6 the Player has alread lost.No need for checking Roulette.
+    # If the dice roll is 1 or 6 the player has already lost. No need for the spin.
     while roll != 1 or roll != 6:
-        # If Roll is not 1 or 6  and the player chose Low style betting then the multiplier will be 0.5x and the number should be between 0 and the number of the player.
-        if multiplier==0.5 and num>=spin:
+        # If the dice spin is not 1 or 6 then if he has choosen low style betting the multiplier is 0.5x and the number in the  spin should be less than the number he chose.
+        if multiplier==1.5 and num>=spin:
             winnings = bet*multiplier
             break
-        # If the Roll is not 1 or 6 and the player chhose High style betting thenn the multiplier will be 1.5x and the number should be greater than the number of the player.
-        elif multiplier==1.5 and num<=spin:
+        # If he chose high style for the betting the multiplier is 1.5x and the number in the spin should be more than the number he chose.
+        elif multiplier==3 and num<=spin:
             winnings = bet*multiplier
             break
-        # If the spin  of the roulette is 0 the it calls for a Bet Double from the Dealer.
+        # If the Dice Roll is 1 or 6 and the spin is 0, this calls for a Double Bet fromt eh Dealer.If the player fails to meet he losses.
         elif spin==0 and roll ==1 or spin==0 and roll ==6:
             bet = bet*2
-            # If the Balnce falls short the player has lost.
             if bet>balance:
                 print("You lost all your money")
                 break
@@ -116,7 +128,7 @@ def winning_conditions(bet,multiplier,roll,spin,num,balance):
     return winnings
 
 
-# Basic Game Loop
+# Basic Game Function Loop for the game to run and easy to ammend in future.
 def game():
     balance = deposit()
     while balance>0:
@@ -130,8 +142,10 @@ def game():
         print(f"You won ${winnings}")
         balance=update_balance(balance,bet,winnings)
         print(f'Your balance is ${balance}')
+        if balance==0:
+            balance=deposit()
 
 
-# Using it so that it can be used as a module in further projects if required
+# It is added so that this code can be used as a module in further projects if required.
 if __name__=="__main__":
     game()
